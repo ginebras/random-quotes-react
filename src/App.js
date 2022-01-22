4fb89646bdf690d30c94647ced6c5361e912bc72
ginebras <alenicofranco@gmail.com>
2022-01-22 18:19:46 -0300

    5 files modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React from "react";
+
+import Buttons from './dashboards/buttons';
+import Footer from './dashboards/footer';
+import QuotesAutor from './dashboards/quotesAutor';
+import QuotesTest from './dashboards/QuotesTest';
+import Footer from './dashboards/footer';
+
 import "./style.css";
 
 export default function App() {
